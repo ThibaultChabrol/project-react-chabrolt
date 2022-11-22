5f646ba2fad10fc310b0e802a89964b869189593
Thilbault Chabrol <tc@sirenergies.com>
2022-11-22 11:30:39 +0100

    repair tp1

--- src/App.js
+++ src/App.js
@@ -7,34 +7,45 @@ const baseURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 const App = () => {
     const [post, setPost] = React.useState(null);
-    const [search, setSearch] = React.useState(null);
+    const [search, setSearch] = React.useState("");
 
     React.useEffect(() => {
         {
             axios.get(baseURL + search).then((response) => {
-                setPost(response.data);
+                setPost(response.data.meals);
             });
         }
     }, [search]);
 
 
-    if (!post) return null;
+    if (!post) {
+        return (
+            <div className="header">
+                <h1>recette de cuisine</h1>
+                <input type="text" name="searchBar" id="searchBar"
+                       placeholder="taper le nom d'un aliment (en anglais)"
+                       value={search}
+                       onChange={(e) => setSearch(e.target.value)}/>
+            </div>
+        )
+    }
     return (
         <div>
             <div className="header">
                 <h1>recette de cuisine</h1>
                 <input type="text" name="searchBar" id="searchBar"
                        placeholder="taper le nom d'un aliment (en anglais)"
+                       value={search}
                        onChange={(e) => setSearch(e.target.value)}/>
-                <div className="mainDiv">
-
-                    {post.meals.map(meal =>
-                        <div className="card">
-                            <Cards meals={meal}/>
-                        </div>
-                    )}
-                </div>
             </div>
+            <div className="mainDiv">
+                {post.map(meal =>
+                    <div className="card">
+                        <Cards meals={meal}/>
+                    </div>
+                )}
+            </div>
+
         </div>
     );
 };
